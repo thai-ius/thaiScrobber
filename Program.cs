7a5b41af8108684a8d1e7f213a674a05ba2c5db8
thai-ius <tsandma@iu.edu>
2026-04-23 14:56:16 -0400

    Added ability to input and save file path

--- Program.cs
+++ Program.cs
@@ -19,10 +19,20 @@ static class Scrobbler
         var offset = localTimeZone.BaseUtcOffset.Hours * 3600;
         const int batchsize = 50;
         
-        var path_to = @"C:\Users\namdn\Documents\.scrobbler.log";
+        var pathTo = "";
+        bool savedPathTo = false;
+        
+        //Checks for saved file path to .scrobbler.log
+        if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "pathTo")))
+        {
+            pathTo = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "pathTo"));
+            savedPathTo = true;
+        }
+        
 
         var user = new Auth();
-
+        
+        //Checks for saved user authentication
         if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "auth.json")))
         {
             user = JsonConvert.DeserializeObject<Auth>(
@@ -46,12 +56,34 @@ static class Scrobbler
         //Input tracks from .scrobbler.log
         var reader = new Reader();
         reader.setStrategy(new AutoScrobbleReader());
-        var tracks = reader.Read(File.ReadAllText(path_to));
-        var chunks = tracks.Chunk(batchsize);
+
+        //Requires valid file path to .scrobbler.log
+        while (pathTo == "")
+        {
+            Console.WriteLine("Paste the path to your file: ");
+            var temp = Console.ReadLine();
+            if (File.Exists(temp))
+            {
+                pathTo = temp;
+            }
+            else
+            {
+                Console.WriteLine($"File at {temp} does not exist.");
+            }
+        }
+        
+        if (!savedPathTo)
+        {
+            Console.WriteLine($"Would you like to save {pathTo} for next time? (Y/N)");
+            var input = Console.ReadLine();
+            if (input == "Y")
+                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "pathTo"), pathTo);
+        }
         
 
-        
-        
+        var tracks = reader.Read(File.ReadAllText(pathTo));
+        var chunks = tracks.Chunk(batchsize);
+
         //Scrobble tracks to Last.FM
         var responses = new List<ScrobbleResponse.Root>();
         foreach(var chunk in chunks)
@@ -60,15 +92,30 @@ static class Scrobbler
             ScrobbleResponse.Root response = session.Scrobble(chunk, offset).Result;
             responses.Add(response);
         }
-        
+
+        int accepted = 0;
+        int ignored = 0; 
         //Display results
         foreach (var response in responses)
         {
             foreach (var scrobble in response.scrobbles.scrobble)
             {
-                Console.WriteLine(scrobble.artist.text + "---" + scrobble.album.text + "---" + scrobble.track.text);
+                if (scrobble.ignoredMessage.code != "0") 
+                    Console.Write("[FAILED] \t");
+                Console.Write(scrobble.artist.text + "\t" + scrobble.album.text + "\t" + scrobble.track.text + "\n");
             }
+
+            accepted += response.scrobbles.attr.accepted;
+            ignored += response.scrobbles.attr.ignored;
         }
+        
+        Console.WriteLine($"Tracks accepted: {accepted} Tracks ignored: {ignored}");
+        Console.WriteLine("Delete scrobble log? Enter Y/N:");
+        
+        if (Console.ReadLine() == "Y")
+            File.Delete(pathTo);
+        
+        
     }
     
 }
